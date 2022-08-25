b77ee0a7aede2d736398fbb5a97f5137abf7b48e
Xingjian Mao <xmao29@uwo.ca>
2022-08-24 23:25:05 -0400

    Upload Test MQTT file
    Upload version for testing MQTT address: $hw/events/device/led-light-instance-01/twin/get
    
    Added a five second sleep time in to the loop

---
+++ led-raspberrypi-test/a.bash
@@ -0,0 +1,3 @@
+sudo make
+sudo docker tag led-light-mapper:v1.1 maojerry0/led-light-mapper:v1.1
+sudo docker push maojerry0/led-light-mapper:v1.1
--- led-raspberrypi/a.bash
+++
@@ -1,3 +0,0 @@
-sudo make
-sudo docker tag led-light-mapper:v1.1 maojerry0/led-light-mapper:v1.1
-sudo docker push maojerry0/led-light-mapper:v1.1
